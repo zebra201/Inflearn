ae62c5faf4194db19ef3afdeb56f65458609a60c
zebra201 <zebra3939@gmail.com>
2022-05-13 19:59:26 +0900

    OrderedDict 실습2

--- infspace/python12(collections모듈)/ex04(orderedDict 모듈2).py
+++ infspace/python12(collections모듈)/ex04(orderedDict 모듈2).py
@@ -35,7 +35,6 @@ dic1['z'] = 100     # z : 100
 dic1['m'] = 200
 dic1['3'] = 300
 dic1['g'] = 400
-
 # OrderedDict로 wrapping 을 하면 기존 딕셔너리와 리스트의
 # 순서를 지키면서 딕셔너리 형태로 관리할 수 있다.
 for k,v in OrderedDict(sorted(dic1.items(), key = sort_by_key)).items():
@@ -45,3 +44,54 @@ print("-" * 50)
 li3 = sorted(dic1.items(), key = sort_by_key)
 print(li3)
 print("-" * 50)
+
+
+# 딕셔너리의 동등성 비교
+# 동등성은 논리적 동등이라는 것을 의미한다. 논리적 동ㅇ등이란 주소는 다르지만
+# 요소들의 값이 순서가 비록 틀리더라도 논리적 동등으로 바라보는 시점이다.
+str1 = "이현호"
+str2 = str()
+str2 = "이현호"
+
+print(id(str1))
+print(id(str2))
+print(str1 == str2)
+
+a = 10
+b = 10
+a += 1
+b += 2
+print(id(a))
+print(id(b))
+print("-" * 50)
+
+# 일반적인 딕셔너리 동등성 비교
+dic1 = {"가":1, "나":2, "다":3}
+dic2 = {"가":1, "다":3, "나":2}
+print(id(dic1))
+print(id(dic2))
+print(dic1 == dic2)
+print("-" * 50)
+
+from collections import OrderedDict
+# orderedDict 는 순서도 확인한다.
+# 일반적인 딕셔너리보다 좀 더 깐깐하고 디테일하게 비교하는 것이 바로
+# OrderedDict의 특징이다.
+ordered_dic1 = OrderedDict({"가":1, "나":2, "다":3})
+ordered_dic2 = OrderedDict({"가":1, "다":3, "나":2})
+ordered_dic3 = OrderedDict({"가":1, "나":2, "다":3})
+print(id(ordered_dic1))
+print(id(ordered_dic2))
+print(ordered_dic1 == ordered_dic2)
+print(ordered_dic1 == ordered_dic3)
+
+# 결론
+# 첫 번째는 OrderedDict 모듈은 딕셔너리의 순서대로 저장하지 않는 방식을 순서대로
+# 저장하는 방식으로 개선되었다. (파이썬 버전이 3.6 이후로는 저장과 출력이
+# OrderedDict와 동일하게 작동을 하고 있지만 2.x 버전에서는 문제점이 발생)
+# 두 번째는 딕셔너리의 동등성 비교에서 일반적인 딕셔너리라는 순서를 유지핮
+# 않아도 해당 키와 값이 동등하다면 True를 리턴하지만, OrderedDict에서는
+# 순서, 키, 값이 무조건 같아야만 True를 리턴한다.
+# OrderedDict 를 사용을 하면 정확한 데이터의 순서나 값을 유지하는데 일반
+# 딕셔너리에 비해서 장점이 있다.
+# 딕셔너리보다는 OrderedDict 모듈을 이용하는 것이 좋다.
