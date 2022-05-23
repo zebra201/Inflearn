1ca72309c4070ae77717507310a3ce85ed2c4322
zebra201 <zebra3939@gmail.com>
2022-05-23 23:42:52 +0900

    counter 모듈 실습 ing

--- infspace/python12(collections모듈)/ex10(counter 모듈).py
+++ infspace/python12(collections모듈)/ex10(counter 모듈).py
@@ -4,5 +4,23 @@
 
 from collections import Counter
 
-text = "A press release is the quickest and easiest way to get free publicity.".lower().split()
-print(Counter(text))
+text1 = "A press release is the quickest and easiest way to get free publicity.".lower().split()
+print(Counter(text1))
+print("-" * 50)
+
+# 문자열을 가지고 리스트를 생성함
+text = list("high school")
+print(text)
+print(type(text))
+# Counter 객체를 만드는데 인자값으로 리스트형을 주었따.
+count = Counter(text)
+print(type(count))
+print(count)
+# 시퀀스 자료형에서 어떤 특수문자의 개수를 알고 싶을 때 사용하면 된다.
+print(count["h"])
+print("-" * 50)
+
+# Counter() 객체를 만들 때, 값=개수 와 같은 형태로 입력도 가능하다
+count = Counter(b=2, c=5, a=3)
+print(count)
+print(sorted(count.elements()))
