39956fb8636c139ae919225a55e330c7479851e6
zebra201 <zebra3939@gmail.com>
2022-05-24 00:15:04 +0900

    Counter 모듈 실습

--- infspace/python12(collections모듈)/ex10(counter 모듈).py
+++ infspace/python12(collections모듈)/ex10(counter 모듈).py
@@ -3,6 +3,7 @@
 # 또한 산술/집합 연산이 가능 (단, 음수 값은 출력하지 않는다.)
 
 from collections import Counter
+from itertools import repeat, chain
 
 text1 = "A press release is the quickest and easiest way to get free publicity.".lower().split()
 print(Counter(text1))
@@ -21,6 +22,36 @@ print(count["h"])
 print("-" * 50)
 
 # Counter() 객체를 만들 때, 값=개수 와 같은 형태로 입력도 가능하다
-count = Counter(b=2, c=5, a=3)
+count = Counter(A=3, b=1, c=5, a=3)
+print(count)
+# elements() 는 Counter 로 주어진 값의 요소에 해당하는 값을 풀어서 반환을 한다.
+# 요소는 무작위적으로 반환하며 요소의 수가 1보다 작을 경우에는
+# elements 메서드는 출력하지 않는다. 대소문자를 구별하며 sorted() 정렬이 되어진다.
+
+print(sorted(count.elements()))
+
+# print(list(repeat([1,2,3],3)))
+# print(list(chain.from_iterable((repeat(number,3) for number in [1,2,3]))))
+print("-" * 50)
+
+a = "가"
+b = "나"
+# 값 = 개수 형태로 Counter() 객체를 만들 때는 문자열은 매개변수로 쓸 수가 없다.
+# 문자열 자체를 맵개변수로 넣어주는 것은 아래와 같이 가능하다.
+count = Counter("가나다라가나다라")
 print(count)
 print(sorted(count.elements()))
+
+print("-" * 50)
+# 아래 문자열은 소문자로 다 바뀌고 단어별로 만들어진 리스트가 반환되어진다.
+text = "abcdefg abcdefg repeat count! you can do it nice to meet you. A Count" + "telephone!!!!"
+text = text.lower().split()
+print(text)
+print(Counter(text))
+
+print("-" * 50)
+count = Counter({"가":4, "나":3})
+print(count)
+print(count.elements())
+# 리스트, 시퀀스 타입으로 element 메서드가 열거해준다.
+print(list(count.elements()))
