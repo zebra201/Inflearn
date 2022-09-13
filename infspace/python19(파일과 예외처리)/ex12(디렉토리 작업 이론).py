41858d24279dce0edacc0fbfde81b184c7a094e5
zebra201 <zebra3939@gmail.com>
2022-09-13 18:46:11 +0900

    파일과 예외처리 이론2

---
+++ infspace/python19(파일과 예외처리)/ex12(디렉토리 작업 이론).py
@@ -0,0 +1,7 @@
+# 이론
+# 현재 파이썬 프로그램이 실행되는 디렉토리를
+# 작업 디렉토리(CVD : Current Working Directory) 라고 한다.
+# 작업 디렉토리를 얻으려면 dir = os.getcwd() 함수를 호출한다.
+# 다음 문장으로 작업 디렉토리를 변경할 수 있다.
+# subdir = "data"
+# os.chdir(subdir)
